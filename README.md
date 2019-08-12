ae2fcb474226b3645eadd91161f81ffc07ef11ac
Jan Petr <honzik.petr@gmail.com>
2019-08-12 17:40:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Babiš v Rohlíku
+
+Pokaždé, když vytáhneš peněženku, vybíráš si, jaký chceš svět. Že seš malej pán? I pár drobáků udělá za pár let pěknou kopu peněz. Vyber si, komu pomáháš stavět hnízdo.
+
+**Babiš v Rohlíku** je plugin pro Google Chrome a Firefox, který nahradí obrázek produktu obrázkem Babiše, pokud daný produkt vyrábí firma spadající do holdingu.
+
+![Babiš v Rohlíku](screens/screen.png)
+
+## Instalace
+
+### Máš Google Chrome?
+1. Najdi rozšíření na [Google Web Store](https://chrome.google.com/webstore/detail/babi%C5%A1-v-rohl%C3%ADku/bphomdbffkdiekkihhmmioegdholgdim)
+2. Klikni na tlačítko "Přidat do Chromu"
+3. Potvrď klepnutím na "Přidat rozšíření"
+
+### Máš Firefox?
+1. Najdi rozšíření na [Firefox Add-ons](https://addons.mozilla.org/en-US/firefox/addon/babis-v-rohliku/)
+2, Klikni na tlačítko "Přidat do Firefoxu"
+3. Potvrď klepnutím na "Přidat"
+
+## Contribution
+
+Pokud najdete jakoukoli chybku nebo budete chtít rozšiřit funkčnost pluginu, neváhejte a vytvořte [nové Issue](https://github.com/JanPetr/babis-v-rohliku/issues/new) nebo, pokud jste developer, pošlete rovnou [Pull Request](https://github.com/JanPetr/babis-v-rohliku/compare).
+
+## Credits
+
+Za grafické podklady díky [Adéle Červíkové](https://www.instagram.com/acervikova/) a [Ondrovi Pospíšilovi](https://poon.cz/webdesigner-kreativec-na-volne-noze/ondrej-pospisil) ❤️
---
+++ js/content.js
@@ -0,0 +1,92 @@
+const RohlikBabisPlugin = {
+	babisBrands: [
+		'vodňanské',
+		'vodňany',
+		'vodňanská',
+		'tatra',
+		'kostelecké uzeniny',
+		'kmotr',
+		'olma',
+		'penam',
+		'papei',
+		'odkolek',
+	],
+	imgUrl: typeof chrome != "undefined" ? chrome.extension.getURL("babis.png") : safari.extension.baseURI + 'babis.png',
+	showImages: function () {
+		// Autocomplete
+		RohlikBabisPlugin.replaceImages('img.Whisperer_image');
+		RohlikBabisPlugin.replaceImages('.whisperer-product__img img');
+		
+		// Catalog / Cart "Don't forget" / Cart "Help us save ..."
+		RohlikBabisPlugin.replaceImages('img.productCard__img');
+		
+		// Favorites / Side cart
+		RohlikBabisPlugin.replaceImages('img.grocery-image-placeholder', 'div.products-table__product', '.products-table__name');
+		
+		// Side cart
+		RohlikBabisPlugin.replaceImages('img.itemImage');
+		
+		// Cart - list of purchased items
+		RohlikBabisPlugin.replaceImages('span img.image');
+		
+		// Detail
+		RohlikBabisPlugin.replaceImages('#productDetail .clickable div span img', '#productDetail', '.redirect_link.active')
+	},
+	replaceImages: function (imgSelector, containerSelector, nameSelector) {
+		document.querySelectorAll(imgSelector).forEach(function (img) {
+			// [src!="' + RohlikBabisPlugin.imgUrl + '"]:not(".babis__processed")
+			if (img.getAttribute('src') === RohlikBabisPlugin.imgUrl) {
+				return;
+			}
+			
+			if (img.classList.contains("babis__processed")) {
+				return;
+			}
+			
+			let productName = img.getAttribute('name') || img.getAttribute('alt');
+			
+			if (!productName) {
+				if (containerSelector && nameSelector) {
+					let titleElement = img.closest(containerSelector).querySelector(nameSelector);
+					if (titleElement) {
+						productName = titleElement.innerText;
+					}
+				}
+			}
+			
+			if (!productName) {
+				return;
+			}
+			
+			productName = productName.trim().toLowerCase();
+			
+			let found = false;
+			for (let i = 0; i < RohlikBabisPlugin.babisBrands.length; i++) {
+				let brand = RohlikBabisPlugin.babisBrands[i];
+				
+				if (RohlikBabisPlugin.wordInString(productName, brand)/* && productName.indexOf(brand) === 0*/) {
+					img.setAttribute('src', RohlikBabisPlugin.imgUrl);
+					found = true;
+				}
+			}
+			
+			if (found === false) {
+				img.classList.add('babis__processed');
+			}
+		});
+	},
+	wordInString: function (s, word) {
+		s = s.replace("(", " ").replace(")", " ");
+		
+		const arr = s.split(/\s+/);
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] === word) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+};
+
+window.setInterval(RohlikBabisPlugin.showImages, 100);
